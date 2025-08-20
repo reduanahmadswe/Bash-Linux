99a91af0c2858532eaa0e156755ee97865fccbf2
Reduan Ahmad <reduanahmad363737@gmail.com>
2025-08-20 22:36:29 +0600

    first commit

---
+++ variable.sh
@@ -0,0 +1,15 @@
+name="Reduan Ahmad"
+echo $name
+
+echo Our shell script is running as user $name
+
+echo Our shell version name is $BASH_VERSION
+echo Our shell version is $BASH
+echo Our home directory is $HOME
+echo Our present working directory is $PWD
+
+name=Reduan
+val=10
+
+echo $name
+echo $val
